362693545b539f7193d6775e2245502d9b4f1bd8
JudithBenelin <jdorathy@in.ibm.com>
2024-06-17 10:48:03 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java/src/main/java/Project/Activities/Activity1/java/App.java
@@ -0,0 +1,13 @@
+package Project.Activities.Activity1.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
