b9c3c2a61a78691947e15b54edcfbe94e7b3d19d
JudithBenelin <jdorathy@in.ibm.com>
2024-09-18 16:28:54 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity7.py
@@ -0,0 +1,19 @@
+# Import webdriver from selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+# Set up the Firefox Driver with WebDriverManger
+service = FirefoxService(GeckoDriverManager().install())
+
+# Start the Driver
+with webdriver.Firefox(service=service) as driver:
+    # Navigate to the URL
+    driver.get("https://v1.training-support.net/selenium/drag-drop")
+
+    # Print the title of the page
+    print("Page title is: ", driver.title)
+
+   
